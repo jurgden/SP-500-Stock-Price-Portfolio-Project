586eb7a2fb911e65e5e614861d1676ba7bc8b60a
Jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-30 17:49:03 +0000

    y

--- main.py
+++ main.py
@@ -81,5 +81,5 @@ num_company = st.slider('Number of Companies', 1, 5)
 if st.button('Show Plots'):
   st.header('Stock Closing Price')
   for i in list(df_selected_sector.Symbol)[:num_company]:
-    st.line_chart(df_selected_sector.Symbol[i].Close)
+    st.line_chart(i.Close)
 
